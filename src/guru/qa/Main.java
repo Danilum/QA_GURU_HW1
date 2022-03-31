bb94f8d90b47713699a01271ffd70e5ff2cfeed0
Danilum <d.shilintsev@innopolis.ru>
2022-03-31 07:10:21 +0300

    new methods

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -3,11 +3,21 @@ package guru.qa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("qa_guru Hello!");// write your code here
-        System.out.println("ko");
+        System.out.println("QA_GURU Hello!");// write your code here
+        System.out.println("dsFSDFSDf   sdfsdf");
     }
 
     public static void method(){
+        System.out.println("hello from Innopolis!");
+        System.out.println("DSFSFSDFSdf");
+    }
+
+    public static void method2(){
+        System.out.println("hello from Innopolis!");
+        System.out.println("DSFSFSDFSdf");
+    }
+
+    public static void method3(){
         System.out.println("hello from Innopolis!");
     }
 }
